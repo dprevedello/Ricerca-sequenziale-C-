21264c9737c037625b240a3f5ab3e7b58145b5e6
Daniele Prevedello <dprevedello86@gmail.com>
2021-02-19 09:23:37 +0000

    Aggiunti i commenti e accorpato il ciclo di generazione dei numeri random e la stampa

--- main.c
+++ main.c
@@ -8,20 +8,21 @@ int main(void) {
     srand(time(NULL));
 
     int numeri[DIM];
-    for(int i=0; i<DIM; i++)
+    for(int i=0; i<DIM; i++){
         numeri[i] = rand() % 1000;
-
-    for(int i=0; i<DIM; i++)
         printf("%d ", numeri[i]);
+    }
 
     int valore;
     printf("\n\nInserire il valore da cercare (tra 0 e 1000): ");
     scanf("%d", &valore);
-
+    
+    /*********** RICERCA SEQUENZIALE ***********/
     int posizione = -1;
     for(int i=0; i<DIM && posizione==-1; i++)
         if(numeri[i]==valore )
             posizione = i;
+    /*******************************************/
 
     if(posizione!=-1)
         printf("Il valore è presente in posizione %d\n", posizione);
